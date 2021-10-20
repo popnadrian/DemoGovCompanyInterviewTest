d697d4ec86a55fa2bf9c47479ef68fdde784e669
Adrian Pop <popnadrian@gmail.com>
2021-10-20 21:31:23 +0300

    unit tests for services

---
+++ Tests/DomainLogic.UnitTests/CompanyReadServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessInterfaces;
+using Moq;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DomainLogic.UnitTests
+{
+    public class CompanyReadServiceTests
+    {
+        private readonly CompanyReadService sut;
+        private readonly Mock<ICompanyRepository> _repository = new();
+
+        public CompanyReadServiceTests()
+        {
+            sut = new CompanyReadService(_repository.Object);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_returns_repository_result()
+        {
+            var expected = new[] { new Company(), new Company(), new Company(), new Company(), }.AsEnumerable();
+            _repository.Setup(x => x.GetAllAsync()).Returns(Task.FromResult(expected));
+            var result = await sut.GetAllAsync();
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_returns_repository_result()
+        {
+            var expected = new Company();
+            _repository.Setup(x => x.GetByIdAsync(21)).Returns(Task.FromResult(expected));
+            var result = await sut.GetByIdAsync(21);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public async Task GetByIsinAsync_returns_repository_result()
+        {
+            var expected = new Company();
+            _repository.Setup(x => x.GetByIsinAsync("a")).Returns(Task.FromResult(expected));
+            var result = await sut.GetByIsinAsync("a");
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Tests/DomainLogic.UnitTests/CompanyWriteServiceTests.cs
@@ -0,0 +1,67 @@
+﻿using DataAccessInterfaces;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DomainLogic.UnitTests
+{
+    public class CompanyWriteServiceTests
+    {
+        private readonly CompanyWriteService sut;
+        private readonly Mock<ICompanyRepository> _repository = new();
+        private readonly Mock<ICompanyValidator> _validator = new();
+
+        public CompanyWriteServiceTests()
+        {
+            sut = new CompanyWriteService(_repository.Object, _validator.Object);
+        }
+
+        [Fact]
+        public async Task AddAsync_invokes_validator()
+        {
+            var item = new Company();
+            await sut.AddAsync(item);
+            _validator.Verify(x => x.ValidateAndThrow(item), Times.Once());
+        }
+
+        [Fact]
+        public async Task AddAsync_invokes_repository_create()
+        {
+            var item = new Company();
+            await sut.AddAsync(item);
+            _repository.Verify(x => x.Create(item), Times.Once());
+        }
+
+        [Fact]
+        public async Task AddAsync_invokes_repository_save()
+        {
+            var item = new Company();
+            await sut.AddAsync(item);
+            _repository.Verify(x => x.SaveChangesAsync(), Times.Once());
+        }
+
+        [Fact]
+        public async Task UpdateAsync_invokes_validator()
+        {
+            var item = new Company();
+            await sut.UpdateAsync(item);
+            _validator.Verify(x => x.ValidateAndThrow(item), Times.Once());
+        }
+
+        [Fact]
+        public async Task UpdateAsync_invokes_repository_create()
+        {
+            var item = new Company();
+            await sut.UpdateAsync(item);
+            _repository.Verify(x => x.Update(item), Times.Once());
+        }
+
+        [Fact]
+        public async Task UpdateAsync_invokes_repository_save()
+        {
+            var item = new Company();
+            await sut.UpdateAsync(item);
+            _repository.Verify(x => x.SaveChangesAsync(), Times.Once());
+        }
+    }
+}
